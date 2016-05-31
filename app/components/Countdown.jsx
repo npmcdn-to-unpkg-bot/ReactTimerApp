2a21268e15878009aa7356ae26e5f31f93081031
AlexTrindade <trindadealex@gmail.com>
2016-05-31 15:13:36 -0300

    Initial commit

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Countdown = (props) => {
+  return (
+    <h3 className="text-center">Countdown.jsx</h3>
+  )
+};
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Timer = (props) => {
+  return (
+    <h3 className="text-center">Timer.jsx</h3>
+  )
+};
+
+module.exports = Timer;
